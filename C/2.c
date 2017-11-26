c1f5252995804927f00e31629e41d5752dd6762a
Zecheng Qian <627374286@qq.com>
2017-11-26 10:50:35 +0800

    update

---
+++ C/2.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2
+//
+//  Created by 钱泽诚 on 2017/10/27.
+//  Copyright © 2017年 钱泽诚. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ C/5.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  5
+//
+//  Created by 钱泽诚 on 2017/10/28.
+//  Copyright © 2017年 钱泽诚. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
